0588bb45cf5747f4520ed4446a4d5a5a8397ba13
Hrishikesh Ballal <hballal@gmail.com>
2017-06-13 15:47:43 +0100

    updated to fix boundaries

---
+++ static/workers/computeBoundaryValue.js
@@ -0,0 +1,14 @@
+importScripts('../js/turf.min.js');
+importScripts('../js/moment.min.js');
+importScripts('../js/rtree.min.js');
+
+function computeBoundaryValue(boundaryGJ, ) {
+
+
+
+    return boundaryValue;
+
+}
+self.onmessage = function(e) {
+    computeBoundaryValue(e.data.boundaryGJ, e.data.grid);
+}
